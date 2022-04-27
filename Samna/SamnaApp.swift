1a218b5bf58a3289f3685cffa6ed789a9597d95e
hafizhMo <hafizh.hizb@gmail.com>
2022-04-27 09:13:39 +0700

    Initial Commit

---
+++ Samna/SamnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamnaApp.swift
+//  Samna
+//
+//  Created by Hafizh Mo on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SamnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
